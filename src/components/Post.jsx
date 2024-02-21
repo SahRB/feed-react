1882551ded26c3d9fe1fbd500fe85f045f513449
SahRB <sabrina7410@hotmail.com.br>
2024-02-21 20:41:14 -0300

    Key

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -42,11 +42,11 @@ export function Post({ author, publishedAt, content }) {
         </time>
       </header>
       <div className={styles.content}>
-        {content.map((line, index) => {
+        {content.map((line ) => {
           if (line.type === 'paragraph') {
-            return <p key={index}>{line.content}</p>;
+            return <p key={line.content}>{line.content}</p>;
           } else if (line.type === 'link') {
-            return <p key={index}><a href="#">{line.content}</a></p>
+            return <p key={line.content}><a href="#">{line.content}</a></p>
           }
         })}
       </div>
@@ -60,7 +60,7 @@ export function Post({ author, publishedAt, content }) {
       <div className={styles.commentList}>
 
         {comments.map(comment => {
-          return <Comment content={comment} />;
+          return <Comment key={comment} content={comment} />;
         })}
       </div>
     </article>
