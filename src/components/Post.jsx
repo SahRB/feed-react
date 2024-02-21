85bcc3b9d8daead79a2bc0a72cdd6ea15c49d01b
SahRB <sabrina7410@hotmail.com.br>
2024-02-21 10:29:22 -0300

    Estado do post (useState)

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,52 +1,57 @@
+import React, { useState } from 'react';
 import styles from './Post.module.css';
-import {Comment} from './Comment';
-import {Avatar} from './avatar';
-import {format, formatDistanceToNow} from 'date-fns';
+import { Comment } from './Comment';
+import { Avatar } from './avatar';
+import { format, formatDistanceToNow } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
 
+export function Post({ author, publishedAt, content }) {
+  const [comments, setComments] = useState([1, 2]);
 
+  const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", { locale: ptBR });
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, { locale: ptBR, addSuffix: true });
 
-export function Post({author, publishedAt, content}){
-const publishedDateFormatted = format (publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {locale: ptBR,} );
-const publishedDateRelativeToNow = formatDistanceToNow(publishedAt,{
-    locale: ptBR,
-    addSuffix: true,
-})
-    return(
-        <article className={styles.post}>
-            <header>
-                <div className={styles.author}>
-                <Avatar hasBorder={true} src={author.avatarUrl} />
-                    <div className={styles.authorInfo}>
-                        <strong>{author.name}</strong>
-                        <span>{author.role}</span>
+  function handleCreateNewComment(event) {
+    event.preventDefault();
+    setComments([...comments, comments.length + 1]);
+  }
 
-                    </div>
-                </div>
-                <time title= {publishedDateFormatted} dateTime={publishedAt.toISOString}>
-                    {publishedDateRelativeToNow}
-                </time>
-            </header>
-            <div className={styles.content}>
-            {content.map(line => {
-                if (line.type === 'paragraph') {
-                  return <p>{line.content}</p>;
-                } else if (line.type === 'link') {
-                  return <p><a href="#">{line.content}</a></p>
-                }
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <Avatar hasBorder={true} src={author.avatarUrl} />
+          <div className={styles.authorInfo}>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
+          </div>
+        </div>
+        <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>
+          {publishedDateRelativeToNow}
+        </time>
+      </header>
+      <div className={styles.content}>
+        {content.map((line, index) => {
+          if (line.type === 'paragraph') {
+            return <p key={index}>{line.content}</p>;
+          } else if (line.type === 'link') {
+            return <p key={index}><a href="#">{line.content}</a></p>
+          }
         })}
-            </div>
-            <form className={styles.commentForm}>
-                <strong>Deixe seu feedback</strong>
-                <textarea placeholder="Deixe seu comentário"/>
-                <footer>
-                <button type="submit">Postar</button>
-                </footer>
-                
-            </form>
-            <div className={styles.commentList}>
-                <Comment/>
-            </div>
-        </article>
-    );
+      </div>
+      <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+        <textarea placeholder="Deixe seu comentário" />
+        <footer>
+          <button type="submit">Postar</button>
+        </footer>
+      </form>
+      <div className={styles.commentList}>
+        <Comment />
+        {comments.map(comment => {
+          return <Comment key={comment} />;
+        })}
+      </div>
+    </article>
+  );
 }
